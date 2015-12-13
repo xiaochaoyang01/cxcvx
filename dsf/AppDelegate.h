570fe8f11fa43550f8174f67eb7122ab365b6f35
xiaochaoyang01 <xiaochaoyang@11bnb>
2015-12-13 10:32:39 +0800

    dsfsf

---
+++ dsf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dsf
+//
+//  Created by 肖朝阳 on 15/12/13.
+//  Copyright © 2015年 肖朝阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dsf/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dsf
+//
+//  Created by 肖朝阳 on 15/12/13.
+//  Copyright © 2015年 肖朝阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
